b5026caa02154f929e127fb531fcb034053b8c6b
sycuuui <102959791+sycuuui@users.noreply.github.com>
2023-10-13 23:14:52 +0900

    test : 통신코드 입력

--- gomgom/src/App.js
+++ gomgom/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Main from './components/main/main';
+import axios from 'axios'
 
 function App(){
   return (
--- gomgom/src/components/Header.jsx
+++ gomgom/src/components/Header.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import logoImg from '../img/gomgom.png';
-
+import axios from 'axios'
 function Header(){
+    const [responseMessage, setResponseMessage] = useState(null);
+    const postData = async () => {
+        console.log("클릭함");
+        try {
+          const data = {
+            title: '제목제목',
+            content: '내용내용',
+          };
+    
+          const response = await axios.post('http://223.130.137.75:8080/test', data);
+          setResponseMessage(response.data.message);
+          console.log("성공");
+        } catch (error) {
+          console.error('데이터를 보내는 중 오류 발생:', error);
+        }
+      }
     return(
         <div>
-            <img src={logoImg} style={{width:"142px",height:"27px,",marginTop:"40px"}}></img>
+            <img onClick={postData} src={logoImg} style={{width:"142px",height:"27px,",marginTop:"40px"}}></img>
         </div>
     )
 }
