02a4f43555fe734ab7b2e5b8015d16e60d6f855c
sycuuui <102959791+sycuuui@users.noreply.github.com>
2023-10-14 17:08:56 +0900

    add : get코드 추가

--- gomgom/src/components/Header.jsx
+++ gomgom/src/components/Header.jsx
@@ -3,24 +3,34 @@ import logoImg from '../img/gomgom.png';
 import axios from 'axios'
 function Header(){
     const [responseMessage, setResponseMessage] = useState(null);
-    const postData = async () => {
-        console.log("클릭함");
-        try {
-          const data = {
-            title: '제목제목',
-            content: '내용내용',
-          };
+    // const postData = async () => {
+    //     console.log("클릭함");
+    //     try {
+    //       const data = {
+    //         title: '제목제목',
+    //         content: '내용내용',
+    //       };
     
-          const response = await axios.post('http://223.130.137.75:8080/test', data);
-          setResponseMessage(response.data.message);
-          console.log("성공");
-        } catch (error) {
-          console.error('데이터를 보내는 중 오류 발생:', error);
-        }
+    //       const response = await axios.get('/test/1', data);
+    //       setResponseMessage(response.data.message);
+    //       console.log("성공");
+    //     } catch (error) {
+    //       console.error('데이터를 보내는 중 오류 발생:', error);
+    //     }
+    //   }
+    const fetchData = async () => {
+      console.log("클릭함");
+      try {
+        const response = await axios.get('http://localhost:3000/test/1'); // Use axios.get for a GET request
+        setResponseMessage(response.data.message);
+        console.log("성공");
+      } catch (error) {
+        console.error('데이터를 가져오는 중 오류 발생:', error);
       }
+    }
     return(
         <div>
-            <img onClick={postData} src={logoImg} style={{width:"142px",height:"27px,",marginTop:"40px"}}></img>
+            <img onClick={fetchData} src={logoImg} style={{width:"142px",height:"27px,",marginTop:"40px"}}></img>
         </div>
     )
 }
