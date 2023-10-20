c14e61371f6903df45a351ff5dcbcd84db55fa2d
suzinlim <122861956+suzinlim@users.noreply.github.com>
2023-10-21 02:20:14 +0900

    feat: 회원가입 페이지 구현

--- gomgom/src/App.js
+++ gomgom/src/App.js
@@ -1,7 +1,10 @@
 import React,{useEffect, useState}  from 'react';
 import './App.css';
 import Main from './components/main/main';
-import axios from 'axios'
+import axios from 'axios';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
 
 function App(){
   const [hello, setHello] = useState('')
@@ -13,7 +16,13 @@ function App(){
   }, []);
   return (
     <div className="App">
-      <Main/>
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+      </BrowserRouter>
       <div>
       백엔드에서 가져온 데이터입니다 : {hello}
       </div>
---
+++ gomgom/src/pages/SignUp.jsx
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+import "../css/SignUp.css";
+import userIcon from "../img/userIcon.png"
+
+
+const SignUp= () => {
+    const [id, setId] = useState('');
+    const [password, setPassword] = useState('');
+    const [selectedImage, setSelectedImage] = useState(userIcon);
+
+    const handleIdChange = (e) => {
+        setId(e.target.value);
+      };
+      
+      const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+      };
+      
+
+    const handleImageUpload = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+          const imageURL = URL.createObjectURL(file);
+          setSelectedImage(imageURL);
+        }
+      };
+
+    return (
+        <div style={{ display: "flex", flexDirection: "column" }}>
+            <Header />
+            <Nav />
+            <div className="separator-line"></div>
+            <p className="signup-heading">회원가입</p>
+            <form className="signup-form">
+                <img className="user-img" src={selectedImage || userIcon} alt="userImage" />
+                <label for="fileInput" className="upload-button">
+                <input type="file" id="fileInput" style={{ display: "none" }} accept="image/*" onChange={handleImageUpload} />
+                사진 등록</label>
+                <p className="id-heading">아이디</p>
+                <input type="input" className="id-input" placeholder="아이디를 입력하세요." onChange={handleIdChange} />
+                <p className="passwd-heading">비밀번호</p>
+                <input type="password" className="passwd-input" placeholder="비밀번호를 입력하세요." onChange={handlePasswordChange} />
+                <button type="submit" className="signup-submit" style={{
+                    background: id && password ? '#FFAB5D' : '#E9E5DA'}}
+                    disabled={!id || !password} // 입력 필드가 비어 있을 때 버튼 비활성화
+                >완료</button>
+            </form>
+        </div>
+    );
+}
+
+export default SignUp;
