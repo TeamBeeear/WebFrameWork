d84a67a45807624ad6606be086248eea82ca6519
suzinlim <122861956+suzinlim@users.noreply.github.com>
2023-10-28 23:35:37 +0900

    feat: 로그인 세션 유지

--- gomgom/src/components/Nav.jsx
+++ gomgom/src/components/Nav.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import { Routes, Route, Link } from "react-router-dom";
 import userIcon from "../img/userIcon.png"
+
 function Nav(){
+    const [userId, setUserId] = useState('');
+
+    useEffect(() => {
+      const userCookie = document.cookie
+        .split('; ')
+        .find(row => row.startsWith('userId='));
+  
+      if (userCookie) {
+        const [, userIdValue] = userCookie.split('=');
+        setUserId(userIdValue);
+      }
+    }, []);
+
     const navStyle = {
         display:"flex",
         flexDirection: "row",
@@ -18,7 +32,8 @@ function Nav(){
         width:"30px",
         height:"30px",
         margin:"auto 0",
-        marginLeft:"20px"
+        marginLeft:"20px",
+        marginRight: "10px"
     }
 
     const linkStyle = {
@@ -34,8 +49,9 @@ function Nav(){
             <Link to="/board" style={linkStyle}><p>게시판</p></Link>
             <Link to="/gomgom-board/detail" style={linkStyle}><p>곰곰이</p></Link>
             <div className="separator-line"></div>
-            <div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
                 <img src={userIcon} style={userIconSizeStyle} alt="" />
+                {userId}님
             </div>
         </div>
     )
