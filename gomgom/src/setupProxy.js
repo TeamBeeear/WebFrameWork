59c515ef2fd1201423333c3feda1f47d2c1716d4
sycuuui <102959791+sycuuui@users.noreply.github.com>
2023-11-03 01:26:23 +0900

    feat : 슬라이더 통신 완료

--- gomgom/src/setupProxy.js
+++ gomgom/src/setupProxy.js
@@ -8,9 +8,17 @@ module.exports = function(app) {
             changeOrigin: true,
         })
     );
-};  
+  
     app.use(
         '/posts',
+        createProxyMiddleware({
+            target: 'http://localhost:8080',
+            changeOrigin: true,
+        })
+    );
+
+    app.use(
+        '/gomgom-post',
         createProxyMiddleware({
             target: 'http://localhost:8080',
             changeOrigin: true,
