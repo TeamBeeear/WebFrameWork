3119318da2f1c4e77b235ca2b8cdaae7d213f44e
suzinlim <122861956+suzinlim@users.noreply.github.com>
2023-11-03 03:27:43 +0900

    refactor: 회원가입, 로그인 스타일

--- gomgom/src/pages/SignUp.jsx
+++ gomgom/src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import Header from "../components/Header";
 import Nav from "../components/Nav";
-import "../css/SignUp.css";
+import "../css/Login.css";
 import userIcon from "../img/userIcon.png"
 import Footer from '../components/Footer';
 
@@ -32,12 +32,21 @@ const SignUp = () => {
         window.location.href = '/login'; // 로그인 페이지로 이동
       };
 
+      const separatorLineStyle = {
+        width: '100%',
+        height: '4px',
+        left: 0,
+        top: '115px',
+        position: 'absolute',
+        background: '#E9E5DA',
+      };
+
       const SignupTitleStyle = {
         color: "#67594C",
         fontSize: '36px',
         fontFamily: "Pretendard",
         fontWeight: 700,
-        marginTop: "7rem",
+        marginTop: "5rem",
         marginBottom: "3rem",
         wordWrap: "break-word"
       }
@@ -46,9 +55,26 @@ const SignUp = () => {
         width: "150px",
         height: "150px",
         objectFit: "cover",
-        borderRadius: "50%", 
+        borderRadius: "50px", 
         display: "block",
         margin: "0 auto"
+      }
+
+      const uploadBtnStyle = {
+        width: '120px',
+        height: '44px',
+        background: '#E9E5DA',
+        border: '1px solid #D2CDBC',
+        borderRadius: '32px',
+        display: 'block',
+        margin: '0 auto',
+        marginTop: '2rem',
+        color: 'rgba(103, 88, 76, 0.50)',
+        fontSize: '15px',
+        fontFamily: 'Pretendard, sans-serif',
+        fontWeight: 700,
+        lineHeight: '44px',
+        wordWrap: 'break-word'
       }
 
       const SignupCategoryStyle = {
@@ -61,33 +87,25 @@ const SignUp = () => {
         marginBottom: "1.5rem"
       }
 
-      // const inputStyle = {
-      //   width: '557px',
-      //   height: '80px',
-      //   background: 'white',
-      //   borderRadius: '24px',
-      //   border: '2px solid #E9E5DA',
-      //   display: 'block',
-      //   margin: '0 auto 22px', // 간격을 마지막에 추가
-      //   marginTop: '10rem',
-      //   color: '#67594C',
-      //   fontSize: '24px',
-      //   fontFamily: 'Pretendard, sans-serif',
-      //   fontWeight: 700,
-      //   lineHeight: '38.40px',
-      //   paddingLeft: '31px',
-      //   wordWrap: 'break-word',
-      // };
-      
-      // const placeholderStyle = {
-      //   color: '#D2CDBC',
-      //   fontSize: '18px',
-      //   fontFamily: 'Pretendard, sans-serif',
-      //   fontWeight: 500,
-      //   lineHeight: '18%',
-      //   wordWrap: 'break-word',
-      // };
+      const signupSubmitStyle = {
+          width: '340px',
+          height: '75px',
+          background: '#FFAB5D',
+          borderRadius: '44px',
+          display: 'block',
+          margin: '0 auto',
+          marginTop: '5rem',
+          marginBottom: '10rem',
+          border: 'none',
 
+          color: 'white',
+          fontSize: '24px',
+          fontFamily: 'Pretendard, sans-serif',
+          fontWeight: 700,
+          lineHeight: '24%',
+          wordWrap: 'break-word',
+          background: id && password ? '#FFAB5D' : '#E9E5DA',
+      }
 
     return (
         <div style={{ display: "flex", flexDirection: "column" }}>
@@ -97,17 +115,18 @@ const SignUp = () => {
             <p style={SignupTitleStyle}>회원가입</p>
             <form className="signup-form">
                 <img style={userImageStyle} src={selectedImage || userIcon} alt="사용자 이미지" />
-                <label for="fileInput" className="upload-button">
+                <label for="fileInput" style={uploadBtnStyle}>
                 <input type="file" id="fileInput" style={{ display: "none" }} accept="image/*" onChange={handleImageUpload} />
                 사진 등록</label>
                 <p style={SignupCategoryStyle}>아이디</p>
                 <input type="input" className="id-input"  placeholder="아이디를 입력하세요." onChange={handleIdChange} />
                 <p style={SignupCategoryStyle}>비밀번호</p>
                 <input type="password" className="passwd-input" placeholder="비밀번호를 입력하세요." onChange={handlePasswordChange} />
-                <button type="submit" className="signup-submit" style={{
-                    background: id && password ? '#FFAB5D' : '#E9E5DA'}}
-                    disabled={!id || !password} // 입력 필드가 비어 있을 때 버튼 비활성화
-                    onClick={handleSignupClick}
+                <button type="submit" className="submit" style={{
+                  marginBottom: "15rem",
+                  background: id && password ? '#FFAB5D' : '#E9E5DA'}}
+                  disabled={!id || !password} // 입력 필드가 비어 있을 때 버튼 비활성화
+                  onClick={handleSignupClick}  
                 >완료</button>
             </form>
             <Footer />
