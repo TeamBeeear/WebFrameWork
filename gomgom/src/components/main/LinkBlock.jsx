1fb08d29c5ab43865fce3e7a7f9f9ad987a36b72
drimh <107299318+drimh@users.noreply.github.com>
2023-11-12 18:05:25 +0900

    feat: main css 수정

--- gomgom/src/components/main/LinkBlock.jsx
+++ gomgom/src/components/main/LinkBlock.jsx
@@ -18,22 +18,40 @@ function LinkBlock(){
                 marginLeft:"15%",
             }}>
                 <div style={{display:"flex",flexDirection:"row"}}>
-                    <img src={bearImg} style={{width:"110px",height:"82px",marginRight:"53px",paddingTop:"16px"}} alt="" />
-                    <div style={{textAlign:"left"}}>
+                    <img src={bearImg} style={{width:"110px",height:"82px",marginRight:"53px"}} alt="" />
+                    <div style={{textAlign:"left", marginTop:'4%'}}>
                         <p style={{
-                            width:"240px",
-                            height:"23px"
-                        }}>고민에 대한 짧은 해답을 듣고 싶다면?</p>
+                            height:"23px",
+                            color: "var(--67594-c, #67594C)", 
+                            fontFamily: 'Pretendard', 
+                            fontSize: '15px', 
+                            fontStyle: 'normal', 
+                            fontWeight: '500', 
+                            lineHeight: '150%', 
+                            textAlign: 'center', 
+                            margin:'0'
+                            }}>고민에 대한 짧은 해답을 듣고 싶다면?</p>
                         <p style={{
-                            width:"156px",
                             height:"32px",
-                            fontSize:"20px"
+                            fontSize:"20px", 
+                            color: 'var(--67594-c, #67594C)', 
+                            fontFamily: 'Pretendard', 
+                            fontSize: '20px', 
+                            fontStyle: 'normal', 
+                            fontWeight: '700', 
+                            lineHeight: '160%', 
+                            margin:'0'
                         }}>곰곰이에게 물어봐~</p>
                     </div>
                 </div>
                 <button 
                 onClick={handleButtonClick}
-                style={{height: "64px",width:"179px",border:"none",borderRadius: "32px",backgroundColor:"#FAF9F6"}}>
+                style={{height: "64px",width:"179px",border:"none",borderRadius: "32px",backgroundColor:"#FAF9F6", color:' var(--67594-c, #67594C)',
+                fontFamily: 'Pretendard', 
+                fontSize: '20px', 
+                fontStyle: 'normal', 
+                fontWeight: '700', 
+                lineHeight: '100%'}}>
                     곰곰이 바로가기</button>
             </div>
         </div>
@@ -41,3 +59,4 @@ function LinkBlock(){
 }
 
 export default LinkBlock;
+
