a446a723a22e030ac58d2dfb359b60138e95b77d
suzinlim <122861956+suzinlim@users.noreply.github.com>
2023-11-13 03:09:29 +0900

    Feat: 고민 나누기 navigate 연결

--- gomgom/src/components/main/MainContent.jsx
+++ gomgom/src/components/main/MainContent.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import bearImg from "../../img/bearHi.png"
+import { useNavigate } from "react-router-dom";
 
 function MainContent(){
+    const navigate = useNavigate();
+
+    const movetoGominClick = () => {
+        navigate("/post");
+    }
+
     return(
         <div style={{backgroundColor:"#67594C",height: "448px",position:"relative"}}>
             <div style={{ display:"flex",
@@ -30,7 +37,8 @@ function MainContent(){
                 </div>
                 <button style={{borderRadius:"32px",backgroundColor:"#FFAB5D",width:"176px",marginTop:"174px",border:"none",
                                 color:"#FFF",fontSize:"20px",fontWeight:"700",
-                                padding:"20px"}}>
+                                padding:"20px"}}
+                        onClick={movetoGominClick}>
                     고민 나누기</button>
             </div>
             <img src={bearImg} style={{width:"919px",height:"164px",position:"absolute",bottom:"0",transform: "translateX(-50%)"}} alt="" />
