65cc7948b526e945f58a2e424bbc0affc88dcb30
drimh <107299318+drimh@users.noreply.github.com>
2023-11-13 16:05:33 +0900

    feat: main css 수정

--- gomgom/src/components/main/MainContent.jsx
+++ gomgom/src/components/main/MainContent.jsx
@@ -25,22 +25,23 @@ function MainContent(){
                         fontWeight: "600",
                         lineHeight: "160%",
                         width:"386px",
-                        height:"32px"}}>인생은 선택의 연속, 하지만 선택은 항상 어려워요!</p>
+                        height:"32px", 
+                        paddingTop:"5%"}}>인생은 선택의 연속, 하지만 선택은 항상 어려워요!</p>
                     <div style={{fontSize: "36px",
                         fontWeight: "700",
                         lineHeight: "160%",
                         width:"390px",
                         height:"140px",
                         marginRight:"80px"}}>
-                        <p>고민하지말고,</p>
-                        <p>곰곰에서 고민을 나눠주세요</p>
+                        <p style={{margin:"0", paddingTop:'10%'}}>고민하지말고,</p>
+                        <p style={{margin:"0"}}>곰곰에서 고민을 나눠주세요</p>
                     </div>
                 </div>
                 <button 
                 onClick={handleButtonClick}
                 style={{borderRadius:"32px",backgroundColor:"#FFAB5D",width:"176px",marginTop:"174px",border:"none",
                                 color:"#FFF",fontSize:"20px",fontWeight:"700",
-                                padding:"20px"}}>
+                                padding:"0px"}}>
                     고민 올리기</button>
             </div>
             <img src={bearImg} style={{width:"919px",height:"164px",position:"absolute",bottom:"0",transform: "translateX(-50%)"}} alt="" />
