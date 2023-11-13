8cd4f1c206e1ca97e45490e77b89a1dda8e85754
suzinlim <122861956+suzinlim@users.noreply.github.com>
2023-11-14 03:01:32 +0900

    Fix: 고민 올리기 카테고리 라디오 버튼 선택 구현

--- gomgom/src/components/GominPost/WorryCategory.jsx
+++ gomgom/src/components/GominPost/WorryCategory.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import Select from "react-select";
-import CustomOption from "../GominPost/WorryCustomOptions";
+import WorryCustomOptions from "../GominPost/WorryCustomOptions";
 
 const WorryCategory = ({ options, handleOptionChange, selectedOption }) => {
     return (
         <Select
-          options={options}
-          components={{ Option: CustomOption }}
-          isSearchable={false}
-          inputId="customSelect"
-          onChange={(selected) => handleOptionChange(selected)}
-          className="categories"
-          placeholder="기타"
-          defaultValue={options.find((option) => option.value === "기타")}
-          value={selectedOption ? options.find((option) => option.id === selectedOption.id) : null}
+            options={options}
+            components={{ Option: WorryCustomOptions }}
+            isSearchable={false}
+            inputId="customSelect"
+            onChange={(selected) => handleOptionChange(selected)}
+            className="categories"
+            placeholder="기타"
+            defaultValue={options.find((option) => option.value === "기타")}
+            value={selectedOption ? options.find((option) => option.id === selectedOption.id) : null}
         />
     );
 };
--- gomgom/src/components/GominPost/WorryCustomOptions.jsx
+++ gomgom/src/components/GominPost/WorryCustomOptions.jsx
@@ -1,19 +1,33 @@
+import React from "react";
+
 // 카테고리 커스텀 컴포넌트
 const WorryCustomOptions = (props) => {
-  const { innerProps, label, isSelected, isDisabled, selectProps, value } = props;
+    const { innerProps, label, isSelected, isDisabled, selectProps, value } = props;
 
-  // 선택된 항목인 경우, checked 클래스 추가
-  const isChecked = isSelected ? "checked" : "";
+    // 선택된 항목인 경우, checked 클래스 추가
+    const isChecked = isSelected ? "checked" : "";
 
-  // 선택된 항목이 비활성화된 경우, disabled 클래스 추가
-  const isDisabledClass = isDisabled ? "disabled" : "";
+    // 선택된 항목이 비활성화된 경우, disabled 클래스 추가
+    const isDisabledClass = isDisabled ? "disabled" : "";
 
-  return (
-    <div {...innerProps} className={`custom-option ${isChecked} ${isDisabledClass}`}>
-      <input type="radio" value={label} name={selectProps.inputId} checked={isSelected}
-        onChange={() => selectProps.onChange({ label, value })} disabled={isDisabled}/>{label}
-    </div>
-  );
+    return (
+        <label
+            htmlFor={`${selectProps.inputId}_${value.id}`} // id를 가져오기 위해 value.id 사용
+            {...innerProps}
+            className={`custom-option ${isChecked} ${isDisabledClass}`}
+        >
+            <input
+                id={`${selectProps.inputId}_${value.id}`}
+                type="radio"
+                value={label}
+                name={selectProps.inputId}
+                checked={isSelected}
+                onChange={() => selectProps.onChange({ label, value, id: value.id })} // id를 value.id로 설정
+                disabled={isDisabled}
+            />
+            {label}
+        </label>
+    );
 };
 
 export default WorryCustomOptions;
