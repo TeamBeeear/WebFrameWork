9aa5a0cc7603bb7a567cb5fe221dd166952b9390
sycuuui <102959791+sycuuui@users.noreply.github.com>
2023-11-13 16:30:27 +0900

    fix : WorriedBoxContainer 컴포넌트 나누기

--- gomgom/src/components/main/WorriedBoxContainer.jsx
+++ gomgom/src/components/main/WorriedBoxContainer.jsx
@@ -1,7 +1,6 @@
 import React,{useEffect, useState} from "react";
-import WorriedBox from "./WorriedBox";
-import axios from 'axios';
 import { Link } from "react-router-dom";
+import WorriedBoxs from "./WorriedBoxs";
 
 function WorriedBoxContainer({
     url
@@ -13,20 +12,7 @@ function WorriedBoxContainer({
         marginBottom:"34px",
         justifyContent:"space-between",
     }
-    const separateDivStyle={
-        display:"flex",
-        flexDirection:"row",
-        justifyContent:"space-between",
-    }
-    const colDivStyle={
-        display:"flex",
-        flexDirection:"column",
-    }
-    const colRightDivStyle={
-        display:"flex",
-        flexDirection:"column",
-        marginLeft:"20px"
-    }
+
     const firstTitleFontStyle={
         // marginTop:"0px",
         color: "#67594C",
@@ -49,13 +35,7 @@ function WorriedBoxContainer({
         lineHeight: "160%",
         margin:"auto 0"
     }
-    const [data, setData] = useState('')
-
-    useEffect(() => {
-        axios.get(url)
-            .then(response => setData(response.data))
-            .catch(error => console.log(error))
-    }, []);
+    
     return(
         <div style={{backgroundColor:"#FAF9F6",
         display:"flex",
@@ -69,48 +49,9 @@ function WorriedBoxContainer({
                     <p style={firstTitleFontStyle}>오늘의 고민들</p>
                     <Link to='/board'><p style={secondTitleFontStyle}>+ 더 많은 고민 보러가기</p></Link>
                 </div>
-                <div style={separateDivStyle}>
-                    <div style={colDivStyle}>
-                        {Array.isArray(data) && data.slice().reverse().map((item, index) => (
-                            index % 2 === 0 ? ( // 짝수번째 요소
-                                <WorriedBox
-                                    key={item.postId}
-                                    postId={item.postId}
-                                    boardId={item.boardId}
-                                    data={item}
-                                    title={item.title}
-                                    content={item.content}
-                                    commentsCount={item.commentsCount}
-                                    heartsCount={item.heartsCount}
-                                    userId={item.userId}
-                                    firstSelectionContent={item.firstSelectionContent}
-                                    secondSelectionContent={item.secondSelectionContent}
-                                    nminutesAgo={item.nminutesAgo}
-                                />
-                            ) : null
-                        ))}
-                    </div>
-                    <div style={colRightDivStyle}>
-                        {Array.isArray(data) && data.slice().reverse().map((item, index) => (
-                            index % 2 === 1 ? ( // 홀수번째 요소
-                                <WorriedBox
-                                    key={item.postId}
-                                    data={item}
-                                    postId={item.postId}
-                                    boardId={item.boardId}
-                                    title={item.title}
-                                    content={item.content}
-                                    commentsCount={item.commentsCount}
-                                    heartsCount={item.heartsCount}
-                                    userId={item.userId}
-                                    firstSelectionContent={item.firstSelectionContent}
-                                    secondSelectionContent={item.secondSelectionContent}
-                                    nminutesAgo={item.nminutesAgo}
-                                />
-                            ) : null
-                        ))}
-                    </div>
-                </div>
+                <WorriedBoxs 
+                    url = {url}
+                />
 
             </div>
             
