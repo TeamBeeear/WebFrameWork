79b6661563eace6d7c5e643b7ff055374b52b0b8
suzinlim <122861956+suzinlim@users.noreply.github.com>
2023-11-21 01:48:41 +0900

    fix: 투표 이미지 오류 수정

--- gomgom/src/components/GomGomBoard/UserGomin.jsx
+++ gomgom/src/components/GomGomBoard/UserGomin.jsx
@@ -7,22 +7,15 @@ import UserGominTitle from "../GomGomBoard/UserGominTitle";
 function UserGomin({
     title,
     content,
-    firstSelectionId,
     firstSelectionContent,
     firstSelectionVotePercentage,
-    secondSelectionId,
     secondSelectionContent,
     secondSelectionVotePercentage,
 }){
     const [selectedOption, setSelectedOption] = useState(null);
-    const [updatedFirstVotePercentage, setUpdatedFirstVotePercentage] = useState(firstSelectionVotePercentage);
-    const [updatedSecondVotePercentage, setUpdatedSecondVotePercentage] = useState(secondSelectionVotePercentage);
-    const [isGetRequest, setIsGetRequest] = useState(true); 
-
     const handleOptionSelect = (option) => {
         setSelectedOption(option);
     }
-
     const gominContent = { 
         width: "46.125rem",
         height: "auto",
@@ -32,57 +25,6 @@ function UserGomin({
         boxShadow: "0px 2px 4px 0px rgba(210, 205, 189, 0.50)", 
         marginLeft:"2.5rem"
     }
-
-    // userId는 세션 저장소의 userId 값을 가져오는 함수
-    function getUserIdFromSessionStorage() {
-        const userId = sessionStorage.getItem('userId'); // 세션 저장소에서 userId 가져오기
-        return userId;
-    }
-
-    // 현재 URL에서 postId 추출하는 함수
-    function getPostIdFromURL() {
-        const searchParams = new URLSearchParams(window.location.search);
-        const postId = searchParams.get('postId'); // URL에서 postId 가져오기
-        return postId;
-    }
-
-    async function sendVoteToServer(userId, postId, selectionId) {
-        const url = `http://localhost:8080/api/vote/${userId}/${postId}/${selectionId}`;
-    
-        try {
-        const response = await fetch(url, {
-            method: 'PUT',
-            headers: {
-            'Content-Type': 'application/json',
-            },
-        });
-    
-        if (response.ok) {
-            const data = await response.json(); // 서버 응답 데이터 파싱
-            return data; // 응답 데이터 반환
-        } else {
-            console.error('투표 저장에 실패했습니다.');
-        }
-        } catch (error) {
-        console.error('네트워크 오류:', error);
-        }
-    }
-
-    const handleVoteClick = async (selectionId) => {
-        const userId = getUserIdFromSessionStorage();
-        const postId = getPostIdFromURL();
-    
-        try {
-        const response = await sendVoteToServer(userId, postId, selectionId);
-        if (response) {
-            setUpdatedFirstVotePercentage(response.updatedFirstVotePercentage);
-            setUpdatedSecondVotePercentage(response.updatedSecondVotePercentage);
-        }
-        setIsGetRequest(false);
-        } catch (error) {
-        console.error('투표 저장에 실패했습니다.', error);
-        }
-    };
 
     return (
         <div style={{paddingTop:"4.25rem", display:"flex", justifyContent:"center", marginBottom:"6.56rem"}}>
@@ -94,18 +36,16 @@ function UserGomin({
                 <UserGominText gominContents = {content}/>
                 <div style={{display:"flex", gap:"1.5rem", justifyContent:"center"}}>
                     <TwoChoice 
-                        gominId={firstSelectionId}
                         gominText={firstSelectionContent}
-                        gominPercent={isGetRequest ? firstSelectionVotePercentage : updatedFirstVotePercentage}
-                        onClick={() => handleVoteClick(firstSelectionId)}
-                        selected={selectedOption === firstSelectionContent}
+                        gominPercent={firstSelectionVotePercentage}
+                        onClick={() => handleOptionSelect(firstSelectionContent)} 
+                        selected={selectedOption === firstSelectionContent} 
                     />
                     <TwoChoice 
-                        gominId={secondSelectionId}
                         gominText={secondSelectionContent}
-                        gominPercent={isGetRequest ? secondSelectionVotePercentage : updatedSecondVotePercentage}
-                        onClick={() => handleVoteClick(secondSelectionId)}
-                        selected={selectedOption === secondSelectionContent}
+                        gominPercent={secondSelectionVotePercentage}
+                        onClick={() => handleOptionSelect(secondSelectionContent)} 
+                        selected={selectedOption === secondSelectionContent} 
                     />
                 </div>
             </div>
