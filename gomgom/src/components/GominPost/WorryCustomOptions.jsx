ccbb1b64b7a4d23232c3b2e841ce9580917a2019
suzinlim <122861956+suzinlim@users.noreply.github.com>
2023-11-20 12:48:52 +0900

    refactor: 전체적인 코드 수정

--- gomgom/src/components/GominPost/WorryCustomOptions.jsx
+++ gomgom/src/components/GominPost/WorryCustomOptions.jsx
@@ -12,17 +12,17 @@ const WorryCustomOptions = (props) => {
 
     return (
         <label 
-            htmlFor={`${selectProps.inputId}_${value}`}
-            {...innerProps} 
+            htmlFor={`${selectProps.inputId}_${value}`} // 해당 label과 input을 연결시키는 데 사용되는 id
+            {...innerProps} // 추가적인 props를 전달하기 위한 spread 연산자 사용
             className={`custom-option ${isChecked} ${isDisabledClass}`}>
             <input
-                id={`${selectProps.inputId}_${value}`}
+                id={`${selectProps.inputId}_${value}`} // input 요소의 ID 값
                 type="radio"
                 value={value}
                 name={selectProps.inputId}
                 checked={isSelected}
                 onChange={() => selectProps.onChange({ label, value })}
-                disabled={isDisabled}
+                disabled={isDisabled} // 비활성화 상태 여부
             />
             {label}
         </label>
--- gomgom/src/components/Signup/InputField.jsx
+++ gomgom/src/components/Signup/InputField.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const InputField = ({ type, placeholder, onChange, value }) => {
-  const inputStyle = {
+    const inputStyle = {
         width: "557px",
         height: "80px",
         background: "white",
@@ -9,7 +9,7 @@ const InputField = ({ type, placeholder, onChange, value }) => {
         border: "2px solid #E9E5DA",
         display: "block",
         margin: "0 auto", // 가운데 정렬
-        marginBottom: "22px", // 각 입력 필드 사이의 간격 조절
+        marginBottom: "22px",
         marginTop: "10px",
         color: "#67594C",
         fontSize: "24px",
@@ -18,18 +18,18 @@ const InputField = ({ type, placeholder, onChange, value }) => {
         lineHeight: "38.40px",
         paddingLeft: "31px",
         wordWrap: "break-word",
-  };
+    };
 
-  return (
-    <input
-      type={type}
-      className="input-field"
-      placeholder={placeholder}
-      onChange={onChange}
-      value={value}
-      style={inputStyle}
-    />
-  );
+    return (
+        <input
+        type={type}
+        className="input-field"
+        placeholder={placeholder}
+        onChange={onChange}
+        value={value}
+        style={inputStyle}
+        />
+    );
 };
 
 export default InputField;
