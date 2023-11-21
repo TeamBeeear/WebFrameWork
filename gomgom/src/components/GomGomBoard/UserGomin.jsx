749666e0e9d73946a70d38c505c14af24717af59
MinseoKangQ <kms02171@naver.com>
2023-11-22 01:07:20 +0900

    Fix: 투표 API

--- gomgom/src/components/GomGomBoard/UserGomin.jsx
+++ gomgom/src/components/GomGomBoard/UserGomin.jsx
@@ -7,15 +7,72 @@ import UserGominTitle from "../GomGomBoard/UserGominTitle";
 function UserGomin({
     title,
     content,
+    firstSelectionId,
     firstSelectionContent,
     firstSelectionVotePercentage,
+    secondSelectionId,
     secondSelectionContent,
     secondSelectionVotePercentage,
 }){
     const [selectedOption, setSelectedOption] = useState(null);
+    const [updatedFirstVotePercentage, setUpdatedFirstVotePercentage] = useState(firstSelectionVotePercentage);
+    const [updatedSecondVotePercentage, setUpdatedSecondVotePercentage] = useState(secondSelectionVotePercentage);
+    const [isGetRequest, setIsGetRequest] = useState(true); 
+
+
     const handleOptionSelect = (option) => {
         setSelectedOption(option);
     }
+
+    function getUserIdFromSessionStorage() {
+        const userId = sessionStorage.getItem('userId');
+        return userId;
+    }
+
+    function getPostIdFromURL() {
+        const searchParams = new URLSearchParams(window.location.search);
+        const postId = searchParams.get('postId');
+        return postId;
+    }
+
+    async function sendVoteToServer(userId, postId, selectionId) {
+        const url = `http://localhost:8080/api/vote/${userId}/${postId}/${selectionId}`;
+
+        try {
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            return data;
+        } else {
+            console.error('투표 저장에 실패했습니다.');
+        }
+        } catch (error) {
+        console.error('네트워크 오류:', error);
+        }
+    }
+
+    const handleVoteClick = async (selectionId) => {
+        const userId = getUserIdFromSessionStorage();
+        const postId = getPostIdFromURL();
+
+        try {
+        const response = await sendVoteToServer(userId, postId, selectionId);
+        if (response) {
+            setUpdatedFirstVotePercentage(response.updatedFirstVotePercentage);
+            setUpdatedSecondVotePercentage(response.updatedSecondVotePercentage);
+        }
+        setIsGetRequest(false);
+        } catch (error) {
+        console.error('투표 저장에 실패했습니다.', error);
+        }
+    };
+
     const gominContent = { 
         width: "46.125rem",
         height: "auto",
@@ -36,16 +93,18 @@ function UserGomin({
                 <UserGominText gominContents = {content}/>
                 <div style={{display:"flex", gap:"1.5rem", justifyContent:"center"}}>
                     <TwoChoice 
+                        gominId={firstSelectionId}
                         gominText={firstSelectionContent}
-                        gominPercent={firstSelectionVotePercentage}
-                        onClick={() => handleOptionSelect(firstSelectionContent)} 
-                        selected={selectedOption === firstSelectionContent} 
+                        gominPercent={isGetRequest ? firstSelectionVotePercentage : updatedFirstVotePercentage}
+                        onClick={() => handleVoteClick(firstSelectionId)}
+                        selected={selectedOption === firstSelectionContent}
                     />
                     <TwoChoice 
+                        gominId={secondSelectionId}
                         gominText={secondSelectionContent}
-                        gominPercent={secondSelectionVotePercentage}
-                        onClick={() => handleOptionSelect(secondSelectionContent)} 
-                        selected={selectedOption === secondSelectionContent} 
+                        gominPercent={isGetRequest ? secondSelectionVotePercentage : updatedSecondVotePercentage}
+                        onClick={() => handleVoteClick(secondSelectionId)}
+                        selected={selectedOption === secondSelectionContent}
                     />
                 </div>
             </div>
